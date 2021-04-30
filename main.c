8109aa0ac04aaa5b8b2cce70ff232d3c813fe72b
henry f <>
2021-04-30 18:04:22 +0000

    added comments too and reordered parts of the yaml parser

--- main.c
+++ main.c
@@ -3,36 +3,48 @@
 #define OUTFILEPATH "out.png"
 #define CONFIG_FILE_PATH "config.yaml"
 
-//yaml map functions
 void yaml_to_object_stings(char filename[32], char *out_paths, char *out_values){
-  //read input file
+  //flags
+  char comment_flag = 0;
+  char path_data_toggle_flag = 0;
+
+  //data
+  char working_char;
+  char yaml_path[16][32];
+  char yaml_value[32];
+
+  //counters
+  int whitespace_count = 0;
+  int yaml_path_place = 0;
+  int yaml_value_place = 0;
+
+  //open input file
   FILE *fp;
   fp = fopen(filename, "r");
 
-  char comment_flag = 0;
-  char path_data_toggle_flag = 0;
-  int whitespace_count = 0;
-  char working_char;
-  char yaml_path[16][32];
-  int yaml_path_place = 0;
-  char yaml_value[32];
-  int yaml_value_place = 0;
-
+  //loop until all chars from the input file have been read
   while(1){
+    //get next char
     working_char = fgetc(fp);
 
+    //check for EOF
     if(feof(fp)){
       break;
     }
 
+    //check for comment char
     if(working_char == '#'){
       comment_flag = 1;
+    //check for newline
     }else if(working_char == '\n'){
-      yaml_value[yaml_value_place] = 0;
+      //end yaml_value and yaml_path with null chars
       yaml_path[whitespace_count/2][yaml_path_place] = 0;
-      
+      yaml_value[yaml_value_place] = 0;
+
+      //check if yaml_value has data if so then print yaml_path data and yaml_value data to screen     
       if(yaml_value_place != 0){
         int i = 0;
+        //loop though all used paths
         while(i <= whitespace_count/2){
           printf("%s", yaml_path[i]);
           if(i <= whitespace_count/2 - 1){
@@ -42,18 +54,24 @@ void yaml_to_object_stings(char filename[32], char *out_paths, char *out_values)
         }
         printf(": %s\n", yaml_value);
       }
-
+      
+      //reset flags
       comment_flag = 0;
       path_data_toggle_flag = 0;
-      yaml_value_place = 0;
-      yaml_path_place = 0;
+
+      //reset counters
       whitespace_count = 0;
+      yaml_path_place = 0;
+      yaml_value_place = 0;
+    //check for space char and increment whitespace_count
     }else if(!comment_flag && working_char == ' '){
       if(!path_data_toggle_flag){
         whitespace_count++;
       }
+    //check for : and set path_data_toggle_flag to true
     }else if(!comment_flag && working_char == ':'){
       path_data_toggle_flag = 1;
+    //process general chars
     }else if(!comment_flag){
       if(path_data_toggle_flag){
         yaml_value[yaml_value_place] = working_char;
@@ -65,6 +83,7 @@ void yaml_to_object_stings(char filename[32], char *out_paths, char *out_values)
     }
   }
 
+  //close input file
   fclose(fp);
 }
 
